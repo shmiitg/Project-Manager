8264b5da6452efe5104145d5eba5029fa32b1f22
Shubham Mondal <shubhamank002@gmail.com>
2022-09-29 00:41:57 +0530

    Change footer ui

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -4,10 +4,9 @@ function Footer() {
     return (
         <footer className="text-center text-lg-start">
             <div className="mt-5">
-                <div className="row align-items-center">
+                <div className="row align-items-center justify-content-center">
                     <div className="col-lg3 col-md-6 mb-4 mb-md-0">
-                        <p className="text-uppercase colorDesign footer-p">Links</p>
-                        <ul className="list-unstyled mb-0">
+                        <ul className="d-flex list-unstyled mb-0 justify-content-center gap-5">
                             <li>
                                 <a
                                     target="_blank"
